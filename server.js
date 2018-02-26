31c422298dbd9217e07d882fbf5148687f4b6f9a
phild6596 <phild6596@gmail.com>
2018-02-26 13:35:26 -0800

    added body-parser code, my required api and html routes

--- server.js
+++ server.js
@@ -2,3 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+var app = express();
+var PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+
+require ('./app/routing/apiRoutes.js')(app);
+require ('./app/routing/htmlRoutes.js')(app);
+
+
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
