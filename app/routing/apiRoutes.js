1c8aad6e1b5021d41b1a1f73b8c189dadd848c29
phild6596 <phild6596@gmail.com>
2018-02-27 14:23:18 -0800

    Started adding the api routes.

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,7 @@
+var friendAnswers = require('../data/friends.js');
+
+module.exports = function (app) {
+    app.get('/api/friends', function (req, res){
+        res.json(friendAnswers);
+    });
+}
