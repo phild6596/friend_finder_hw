b77755a6e168d604307eebe946407298e9eaf358
phild6596 <phild6596@gmail.com>
2018-02-28 10:15:08 -0800

    populated the friends.js file with data

---
+++ app/data/friends.js
@@ -0,0 +1,73 @@
+let friendAnswers = [
+    {
+        'name':'Phil',
+        'photo':'http://cdn.akc.org/Marketplace/Breeds/Boston_Terrier_SERP.jpg',
+        'scores': [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    },
+    {
+        "name": "Samantha",
+        "photo": 'https://oceanmhs.org/wp-content/uploads/2018/01/starrynight.jpg',
+        "scores": [
+            "5",
+            "4",
+            "3",
+            "5",
+            "4",
+            "3",
+            "3",
+            "2",
+            "4",
+            "4"
+        ]
+    },
+    {
+        "name": "Wonder Woman",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcxM3c1Eyr8BIMgWN1kAgOO0B348YSxQyP2MCtbywCFfjkNaE9KA",
+        "scores": [
+            "1",
+            "5",
+            "5",
+            "4",
+            "1",
+            "5",
+            "5",
+            "1",
+            "5",
+            "3"
+        ]
+    },
+    {
+        "name": "Juan Cena",
+        "photo": "https://i.ytimg.com/vi/2fcCuNVTDco/maxresdefault.jpg",
+        "scores": [
+            "3",
+            "3",
+            "3",
+            "4",
+            "4",
+            "3",
+            "4",
+            "2",
+            "5",
+            "5"
+        ]
+    },
+    {
+        "name": "Jesus",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgp2HdUh7uQt4sBvpNYgQBwbiS9RpPzD0TrAcRzxQYJcbHOxocrA",
+        "scores": [
+            "5",
+            "4",
+            "4",
+            "3",
+            "3",
+            "4",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]
+    }
+]
+
+module.exports = friendAnswers;
